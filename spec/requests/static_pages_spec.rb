05a974ff33555eaf9cffdd8b6512749a052a8b43
Conner <connertolley@gmail.com>
2013-06-26 15:55:23 -0500

    add dynamic ruby content

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,12 @@ describe "Static pages" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+    end
+    
   end
 
 
@@ -17,6 +23,12 @@ describe "Static pages" do
       visit '/static_pages/about'
       page.should have_content('About Us')
     end
+    
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+    end
+    
   end
 
 end
